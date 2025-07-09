e262e9ef5a44847cf276cdc923e68c9380ba45e1
DiegoDev <144474823+DiegoDev2@users.noreply.github.com>
2025-07-09 19:11:34 -0400

    Update cmdr-ai.rb

--- cmdr-ai.rb
+++ cmdr-ai.rb
@@ -1,7 +1,7 @@
 class CmdrAi < Formula
     desc "AI-powered terminal copilot for automatic error fixing"
     homepage "https://github.com/DiegoDev2/cmdr.ai"
-    url "https://github.com/DiegoDev2/cmdr.ai/archive/refs/tags/v1.0.0.tar.gz"
+    url "https://github.com/DiegoDev2/cmdr.ai/archive/refs/tags/v1.0.1.tar.gz"
     sha256 "af1b73ca09f7fb14d7311692ec8eeea8560079cd171ccc1ab99fd093d8e16adb"
     license "MIT"
   
